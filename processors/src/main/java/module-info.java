a3d9901f1366c00907f2056e4d833a77e68ba32c
Pavel <Pavel.Vlasov@nasdanika.org>
2024-05-23 03:29:55 -0400

    Dependencies cleanup

--- processors/src/main/java/module-info.java
+++ processors/src/main/java/module-info.java
@@ -1,9 +1,7 @@
 module org.nasdanika.models.graph.processors {
 		
 	requires transitive org.nasdanika.graph.model;
-	requires transitive org.nasdanika.models.ecore.graph;
 	requires transitive org.nasdanika.models.ncore.processors;
-	requires transitive org.nasdanika.html.model.app.graph;
 	
 	exports org.nasdanika.models.graph.processors.ecore;
 	opens org.nasdanika.models.graph.processors.ecore; // For loading resources
