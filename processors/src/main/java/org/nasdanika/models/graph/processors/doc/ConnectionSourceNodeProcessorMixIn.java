53e2988c455d158a1626d985e40b36243829bb47
Pavel <Pavel.Vlasov@nasdanika.org>
2024-07-13 12:38:47 -0400

    Connections anonymous

--- processors/src/main/java/org/nasdanika/models/graph/processors/doc/ConnectionSourceNodeProcessorMixIn.java
+++ processors/src/main/java/org/nasdanika/models/graph/processors/doc/ConnectionSourceNodeProcessorMixIn.java
@@ -2,10 +2,47 @@
  */
 package org.nasdanika.models.graph.processors.doc;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EReference;
+import org.nasdanika.common.ProgressMonitor;
+import org.nasdanika.graph.emf.EReferenceConnection;
 import org.nasdanika.graph.model.ConnectionSource;
+import org.nasdanika.graph.model.ModelPackage;
+import org.nasdanika.html.model.app.Action;
+import org.nasdanika.html.model.app.Label;
+import org.nasdanika.html.model.app.graph.WidgetFactory;
+import org.nasdanika.html.model.app.graph.emf.OutgoingReferenceBuilder;
 
 public interface ConnectionSourceNodeProcessorMixIn<T extends ConnectionSource<?>> extends GraphElementNodeProcessorMixIn<T> {
-
-//	EList<C> getOutgoingConnections();
+	
+	@OutgoingReferenceBuilder(
+			nsURI = ModelPackage.eNS_URI,
+			classID = ModelPackage.CONNECTION_SOURCE,
+			referenceID = ModelPackage.CONNECTION_SOURCE__OUTGOING_CONNECTIONS)
+	default void buildOutgoingConnectionsOutgoingReference(
+			EReference eReference,
+			List<Entry<EReferenceConnection, WidgetFactory>> referenceOutgoingEndpoints, 
+			Collection<Label> labels,
+			Map<EReferenceConnection, Collection<Label>> outgoingLabels, 
+			ProgressMonitor progressMonitor) {
+		
+		for (Label tLabel: labels) {
+			if (tLabel instanceof Action) {
+				EList<Action> tLabelAnonymous = ((Action) tLabel).getAnonymous();
+				outgoingLabels
+					.values()
+					.stream()
+					.flatMap(Collection::stream)
+					.filter(Action.class::isInstance)
+					.map(Action.class::cast)
+					.forEach(tLabelAnonymous::add);
+			}
+		}		
+	}	
 
 } 
